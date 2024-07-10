730b76a1d9bab691720b13535b79a711af372edd
md-hasibul-hasan <md.hasibul.hasan@awl.co.jp>
2024-07-11 02:02:53 +0900

    update: unit tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "example.h"
+#include "class_example.h"
 
 TEST(SampleAppTest, NameTest) {
     ASSERT_EQ("Hasib", GetAuthor());
@@ -7,4 +8,12 @@ TEST(SampleAppTest, NameTest) {
 
 TEST(SampleAppTest, NameNotNull) {
     ASSERT_NE("", GetAuthor());
+}
+
+TEST(ClassExampleTest, ConstructorCheck) {
+    ClassExampleUser user;
+    ASSERT_EQ("Hasib", user.GetUser().name);
+    ASSERT_EQ(29, user.GetUser().age);
+    ASSERT_EQ("062-0006", user.GetUser().post_code);
+    ASSERT_EQ("Sapporo", user.GetUser().address);
 }
